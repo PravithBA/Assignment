f00904018b2c469a119698f08580b908a2ec7022
SingleSimha <pravithba10@gmail.com>
2022-07-02 20:59:24 +0530

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  } from '@fortawesome/free-brands-svg-icons';
+import { faCirclePlay, faPlusSquare, faSquarePlus } from '@fortawesome/free-regular-svg-icons';
+import { useEffect, useState } from 'react';
+import data from './data.json'
 
 function App() {
+  document.title = 'Team Manager'
+  const [tasks,setTasks] = useState({})
+  useEffect(()=>{
+    console.log()
+  },[])
+  const addTask = ()=>{
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main">
+        <div className='table'>
+          <nav className="header">
+            <h1>Sales log</h1>
+            <button className='add-task-btn'>
+              <FontAwesomeIcon icon={faSquarePlus} style={{fontSize:"1em"}} />
+              Add New Task
+            </button>
+            <input className='search-bar' placeholder='Search ' />
+          </nav>
+          <div className="task-container">
+
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
